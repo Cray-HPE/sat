cc0ee5df953e6c4de25944d53187e8d2bc2d9f91
Ryan Haasken <haasken@cray.com>
2019-08-29 17:29:32 -0500

    CRAYSAT-24: Add example unit test and DST scripts
    Add example unit test and scripts `runBuildPrep.sh` and `runUnitTest.sh`
    to run the unit tests in the DST pipeline.
    
    Test Description:
    Tested that the scripts worked locally in a python virtual environment.
    Pushed branch and verified that the build succeeded.

---
+++ runUnitTest.sh
@@ -0,0 +1,5 @@
+# Run nosetests with the options that are in setup.cfg
+# TODO: It is not clear what the difference is between runCoverage.sh and
+# runUnitTest.sh. We run unit tests and compute coverage here. We'll have
+# to figure out how to actually do something with our coverage data.
+nosetests
