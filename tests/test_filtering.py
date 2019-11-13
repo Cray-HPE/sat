Cray-HPE/sat

commit 9837bedede3dbd5753bc0eff62ac0782cbd8ce13

CRAYSAT-142: Implement output filtering.
This commit adds utilities for filtering output against specific
queries specified on the command line. This is done using a simple
'query language' which allows for comparisons of attributes against
values, and combinations of these using logical boolean
operators ('and' and 'or'.) Values can be wildcards, string
literals (enclosed in single- or double-quotes), or numbers. A simpler
version of the query language was also implemented which is roughly
equivalent to the filtering mechanism used in XC products such as
`cnode` or `cmap`.

This commit also adds tests for the new functionality, as well as an
ArgumentParser which can be used as a parent parser for subcommand
parsers so that the filtering options can be generalized across these
subcommands.

Test Description:
1. Run unit tests to ensure they pass.
2. At the moment, this code has not yet been integrated with the
   tools as they currently function; however, once they are, filtering
   can be tested on 'real hardware'.

sat/filtering.py
@@ -1,0 +1,452 @@
+"""
+Generic output filtering utilities for SAT.
+
+Copyright 2019 Cray Inc. All rights reserved.
+"""
+
+import fnmatch
+import logging
+import operator
+
+from parsec import ParseError
+import parsec
+
+LOGGER = logging.getLogger(__name__)
+
+# Note: this comparator matching group is order-dependent because
+# Python's re module is very silly and does not use maximal munch.
+COMPARATOR_RE = r'(>=|<=|<|>|!=|=)'
+
+
+class FilterFunction:
+    """A callable object which implements a filtering function.
+
+    This function essentially emulates a closure which uses the
+    query_key and value parsed from some query string to filter
+    an input dictionary. A value can be a number (int or float)
+    or a string.
+
+    The 'query_key' argument passed to the constructor can be any
+    subsequence of the desired key to filter against. The actual
+    underlying dictionary key is computed the first time the function
+    is called. This optimization is made since it is assumed that
+    headings (i.e. keys) are identical for each dictionary in the
+    iterable being filtered.
+    """
+
+    def __init__(self, query_key, comparator, cmpr_val):
+        """Creates a new FilterFunction.
+
+        Args:
+            query_key: a subsequence of some key to filter against.
+            comparator: a comparator string (i.e. =, !=, >, <, >=,
+                or <=)
+            cmpr_val: a string or number which defines the filter.
+        """
+        self._cmpr_fn = _get_cmpr_fn(comparator, is_number=isinstance(cmpr_val, float))
+        self._cmpr_val = cmpr_val
+
+        # The true key will be computed on the first run, so just
+        # store whatever we're given here for now.
+        self._raw_query_key = query_key
+
+    def __call__(self, row):
+        """Checks whether the given row matches the filter.
+
+        Note that this function has side-effects; the query_key passed
+        to the constructor can be a subsequence of the actual
+        dictionary key to be filtered against, thus the underlying
+        dictionary key will be computed based on the keys present in
+        the first dictionary being filtered. This key will be stored
+        and used in future comparisons. Care should be used if the
+        same filter is applied to multiple lists with differing
+        headers.
+
+        Args:
+            row: a dictionary which is to be filtered.
+
+        Returns:
+            True if row matches the filter, False otherwise.
+        """
+        if not hasattr(self, '_computed_query_key'):
+            self._computed_query_key = \
+                _match_query_key(self._raw_query_key, row.keys())
+
+        try:
+            return self._cmpr_fn(row[self._computed_query_key],
+                                 self._cmpr_val)
+
+        except TypeError as err:
+            raise TypeError("Cannot filter value of type '{}' with value "
+                            "of type '{}'.".format(type(row[self._computed_query_key]).__name__,
+                                                   type(self._cmpr_val).__name__)) from err
+
+
+def combine_filter_fns(fns, combine_fn=all):
+    """Creates a function which is the logical combination of input functions.
+
+    Args:
+        fns: iterable of functions which take a single argument
+            and return a bool.
+        combine_fn: a function which takes an iterable of booleans and
+            returns a boolean. By default, this is `all`. (i.e., the
+            logical 'and' of the input functions. For a logical 'or'
+            behavior, `any` can be passed instead.
+
+    Returns:
+        a function which takes a single argument and returns
+        the logical combination of all argument functions according to
+        combine_fn.
+    """
+    def inner(x):
+        return combine_fn(fn(x) for fn in list(fns))
+    return inner
+
+
+def _subsequence(needle, haystack):
+    """Checks if needle is a subsequence of haystack.
+
+    Informally, needle is a subsequence of haystack if needle can be
+    obtained by deleting zero or more characters from haystack.
+
+    Formally, let haystack be some sequence of characters h_1, h_2,
+    ..., h_k, where k = len(haystack), and let s be some strictly
+    increasing sequence of length l, where 0 <= l <= k, and for any i
+    in s, 1 <= i <= k. Then needle is the sequence of characters
+    h_(s_1), h_(s_2), ..., h_(s_l).
+
+    Args:
+        needle: the subsequence to look for
+        haystack: the string in which to search for the subsequence
+
+    Returns:
+        True if needle is a subsequence of haystack, and False
+        otherwise.
+    """
+    if needle and haystack:
+        try:
+            first, rest = needle[0], needle[1:]
+            hpos = haystack.index(first)
+            return _subsequence(rest, haystack[(hpos + 1):])
+
+        except ValueError:
+            # Letter from needle not found in haystack, so there's no
+            # way it's a subsequence.
+            return False
+
+    else:
+        return not needle
+
+
+def _match_query_key(query_key, headings):
+    """Computes the underlying key from some user-supplied query.
+
+    If query_key is the subsequence of exactly one heading in
+    headings, then that heading is returned. Otherwise, a KeyError is
+    raised.
+
+    Args:
+        query_key: a string containing some key we want to match
+        headings: an iterable containing various headings
+
+    Returns:
+        the unique key matching query_key.
+
+    Raises:
+        KeyError: if zero or multiple headings are matched
+    """
+    # We want to be able to match on just the subsequence of
+    # query_key, since keys can be somewhat long or have extraneous
+    # info (e.g. units etc.) that we don't want the user to worry
+    # about.
+    matching_keys = [key for key in headings
+                     if _subsequence(query_key.lower(), key.lower())]
+    if len(matching_keys) != 1:
+        raise KeyError("Query key '{}' is invalid because it {}"
+                       .format(query_key,
+                               ('does not exist.' if not matching_keys
+                                else 'is ambiguous. (could be one of: {})'
+                                .format(', '.join(matching_keys)))))
+    return matching_keys.pop()
+
+
+def _str_eq_cmpr(name, pattern):
+    """Compares name to pattern with wildcards.
+
+    Comparison is case insensitive. Pattern matching is based on the
+    fnmatch module.
+
+    Args:
+        name (str): some value to check.
+        pattern (str): a wildcard pattern which might
+            match name.
+
+    Returns:
+        bool: True if name matches the pattern after wildcard
+            expansion, and False otherwise.
+    """
+    return fnmatch.fnmatch(str(name).lower(),
+                           pattern.lower())
+
+
+def _get_cmpr_fn(fn_sym, is_number=False):
+    """Returns a comparator function given some symbol.
+
+    Comparator functions are built-in operators for >, >=, <, <=, =,
+    and !=. For =, if is_number is True, then the built-in equals
+    operator is returned. Otherwise, a wildcard matching function is
+    returned.
+
+    If fn_sym is an unrecognized operator, ValueError is raised.
+
+    Args:
+        fn_sym: a character containing a comparison symbol.
+        is_number: whether the given function should just compare
+            numbers or strings.
+
+    Returns:
+        a function which implements the given comparator.
+
+    Raises:
+        ValueError: if fn_sym is not a valid operator.
+    """
+    fns = {
+        '>':   operator.gt,
+        '>=':  operator.ge,
+        '<':   operator.lt,
+        '<=':  operator.le,
+        '!=': (operator.ne if is_number
+               else lambda n, p: not _str_eq_cmpr(n, p)),
+        '=':  (operator.eq if is_number
+               else _str_eq_cmpr)
+    }
+
+    if fn_sym not in fns:
+        raise ValueError('Invalid comparison symbol')
+
+    return fns.get(fn_sym)
+
+
+def parse_query_string(query_string):
+    """Compiles a query string into a function for filtering rows.
+
+    If query_string is invalid, ParseError is raised.
+
+    Args:
+        query_string: a string against which the rows should be
+            filtered
+
+    Returns:
+        a function which returns True if a given row matches
+        the query string, and False otherwise.
+
+    Raises:
+        ParseError: if query_string is not a valid query.
+    """
+
+    def lexeme(p):
+        """Creates subparsers (potentially) surrounded by whitespace.
+
+        Args:
+            p: a parsec.Parser object
+
+        Returns:
+            a parser which is followed by optional whitespace.
+        """
+        whitespace = parsec.regex(r'\s*')
+        return p << whitespace
+
+    tok_dq = lexeme(parsec.string('"'))
+    tok_sq = lexeme(parsec.string('\''))
+    tok_and = lexeme(parsec.string('and'))
+    tok_or = lexeme(parsec.string('or'))
+    tok_cmpr = lexeme(parsec.regex(COMPARATOR_RE))
+    tok_lhs = lexeme(parsec.regex(r'[a-zA-Z_]+'))
+
+    @lexeme
+    @parsec.generate
+    def tok_double_quoted_str():
+        """Parses a double-quoted string.
+
+        Double-quoted strings can contain any non-double-quote
+        character.
+
+        Returns:
+            a string containing the contents of the quoted string.
+        """
+        yield tok_dq
+        content = yield parsec.regex(r'[^"]*')
+        yield tok_dq
+
+        return content
+
+    @lexeme
+    @parsec.generate
+    def tok_single_quoted_str():
+        """Parses a single-quoted string.
+
+        Single-quoted strings can contain any non-single-quote
+        character.
+
+        Returns:
+            a string containing the contents of the quoted string.
+        """
+        yield tok_sq
+        content = yield parsec.regex(r'[^\']*')
+        yield tok_sq
+
+        return content
+
+    tok_quoted_str = tok_double_quoted_str ^ tok_single_quoted_str
+
+    @lexeme
+    @parsec.generate
+    def tok_rhs():
+        """Parse the right hand side of an expression.
+
+        The right hand side can be a number or some wildcard. Numbers
+        are parsed into floats, and wildcards are returned as
+        strings. These are handled separately from quoted strings,
+        which are always interpreted as strings.
+
+        Returns:
+             a float if the value can be parsed as a number, or a
+             string otherwise.
+        """
+        content = yield lexeme(parsec.regex(r'(\w|[*?.])+'))
+        try:
+            return float(content)
+        except ValueError:
+            return content
+
+    @parsec.generate
+    def comparison():
+        r"""Parses a comparison expression (e.g. 'foo=bar')
+
+        Comparison expressions have the following grammar, in pseudo-BNF:
+            <ident> ::= tok_lhs
+            <single_quoted_str> ::= ' <str> '
+            <double_quoted_str> ::= " <str> "
+            <wildcard> ::= tok_rhs
+            <num> ::= FLOAT_RE
+            <comparator> ::= '>=' | '>' | '<' | '<=' | '=' | '!='
+            <cmpr_val> ::= <wildcard> | <num>
+            <comparison> ::= <ident> <comparator> <cmpr_val>
+
+        If the given value is a string, then the value in the
+        row will be filtered using fnmatch.fnmatch (i.e.,
+        wildcards will be expanded.) If the value is instead a
+        number, a numerical comparison will be used.
+
+        Returns:
+            a function which can filter rows according to the
+            comparison sub-expression which this parser parses.
+        """
+        # TODO: It might be a "good" idea in the future to refactor
+        # the grammar a little bit to enforce types on certain
+        # comparisons (e.g., only allow comparisons to numbers for
+        # greater-than or less-than), but if this doesn't turn out to
+        # be an issue, it probably isn't all that necessary.
+        query_key = yield tok_lhs
+        comparator = yield tok_cmpr
+        cmpr_val = yield (tok_rhs ^ tok_quoted_str)
+
+        return FilterFunction(query_key, comparator, cmpr_val)
+
+
+    @parsec.generate
+    def bool_or_expr():
+        """Parses an 'or' expression. (e.g. 'foo = bar or baz > 10')
+
+        'or' expressions have the following grammar:
+            or_expr   ::= and_expr | <or_expr> "or" <and_expr>
+
+        Returns:
+            a function which can filter input rows by whether
+            they match the boolean expression according to its
+            constituent parts and operator.
+        """
+        lhs = yield or_expr
+        yield tok_or
+        rhs = yield and_expr
+        return combine_filter_fns([comparison, and_expr],
+                                   combine_fn=any)
+
+    @parsec.generate
+    def bool_and_expr():
+        """Parses an 'and' expression. (e.g. 'foo = bar and baz > 10')
+
+        'and' expressions have the following grammar:
+            and_expr   ::= comparison | <and_expr> "and" <comparison>
+
+        Returns:
+            a function which can filter input rows by whether
+            they match the boolean expression according to its
+            constituent parts and operator.
+        """
+        lhs = yield and_expr
+        yield tok_and
+        rhs = yield comparison
+        return combine_filter_fns([comparison, and_expr])
+
+    # This should enforce operator precedence at the grammar level.
+    and_expr = comparison ^ bool_and_expr
+    or_expr = and_expr ^ bool_or_expr
+
+    @parsec.generate
+    def bool_expr():
+        # Left hand side is always a comparison. (i.e. boolean
+        # operators are always right-associative.)
+        lhs = yield comparison
+        oper = yield (tok_and | tok_or)
+
+        # Right hand side can be another boolean expression; if it
+        # isn't, fall back and check if it's just a single comparison
+        # (i.e., the base case)
+        rhs = yield (bool_expr ^ comparison)
+
+        return combine_filter_fns([lhs, rhs],
+                                  combine_fn=all if oper == 'and' else any)
+
+    # Expressions can either be a boolean expression composing >= 2
+    # comparisons, or just a single comparison.
+    expr = bool_expr ^ comparison
+
+    return expr.parse_strict(query_string)
+
+
+def filter_list(dicts, query_strings):
+    """Filters a list of dicts according to some query strings.
+
+    If the filter string is invalid, then dicts will be returned as a
+    list, contents unchanged. It is assumed that every dict in dicts
+    will have identical keys. If not, ValueError will be raised.
+
+    Args:
+        dicts: a list or iterable of OrderedDicts which is to be
+            filtered.
+        query_strings: an iterable of some query strings against
+            which to filter the input list.
+
+    Returns:
+        a list dicts filtered according to query_string.
+
+    Raises:
+        ValueError: if keys in dicts are inconsistent.
+        ParseError: if any of query_strings is invalid.
+        KeyError: if attempting to filter against an invalid
+            key.
+    """
+    if not dicts:
+        return []
+
+    # Assume the first row's headings are the "right ones."
+    first, rest = dicts[0], dicts[1:]
+    fkeys = first.keys()
+    if any(d.keys() != fkeys for d in rest):
+        raise ValueError('All input dicts must have same keys.')
+
+    all_filter_fns = [parse_query_string(query_string)
+                      for query_string in query_strings]
+    filter_fn = combine_filter_fns(all_filter_fns)
+    return list(filter(filter_fn, dicts))

sat/hwinv/system.py
@@ -9,6 +9,7 @@ import logging
 import inflect
 
 from sat.config import get_config_value
+from sat.filtering import filter_list
 from sat.hwinv.constants import EMPTY_STATUS, STATUS_KEY, TYPE_KEY
 from sat.hwinv.chassis import Chassis
 from sat.hwinv.compute_module import ComputeModule
@@ -193,8 +194,8 @@ class System:
                     comp_dict.values(), fields)
             else:
                 list_key = '{}_list'.format(object_type.arg_name)
-                all_lists[list_key] = self.get_components_as_dicts(
-                    comp_dict.values(), fields)
+                all_lists[list_key] = filter_list(self.get_components_as_dicts(
+                    comp_dict.values(), fields), self.args.filter_str)
 
         return all_lists
 
@@ -251,7 +252,8 @@ class System:
             report = Report(component_list[0], component,
                             self.args.sort_by, self.args.reverse,
                             get_config_value('format.no_headings'),
-                            get_config_value('format.no_borders'))
+                            get_config_value('format.no_borders'),
+                            filter_strs=self.args.filter_strs)
             report.add_rows(component_list[1:])
 
             full_list_string += str(report.get_pretty_table()) + '\n\n'

sat/parsergroups.py
@@ -40,3 +40,20 @@ def create_format_options():
               'or a 0-based index. Only applies for "pretty"'))
 
     return parser
+
+def create_filter_options():
+    """Creates a parser containing options for filtering.
+
+    Returns: an ArgumentParser object configured with options and help
+        text for filtering.
+    """
+    parser = ArgumentParser(add_help=False)
+
+    group = parser.add_argument_group(
+        'filtering options', 'Options to filter output for applicable commands.')
+
+    group.add_argument(
+        '--filter', metavar='QUERY', dest='filter_strs', action='append',
+        help='Filter the output against some constraint.')
+
+    return parser

sat/report.py
@@ -8,6 +8,7 @@ import logging
 
 from prettytable import PrettyTable
 
+from sat.filtering import filter_list, ParseError
 from sat.util import yaml_dump, get_rst_header
 
 
@@ -19,7 +20,8 @@ class Report:
     """
     def __init__(self, headings, title=None,
                  sort_by=None, reverse=False,
-                 no_headings=False, no_borders=False, align='l'):
+                 no_headings=False, no_borders=False,
+                 align='l', filter_strs=None):
         """Create a new SatTable instance.
 
         Args:
@@ -33,6 +35,8 @@ class Report:
                 headings from the display.
             no_borders: If True, then omit the borders around the table's cells.
             align: Set the alignment within cells. Defaults to left-alignment.
+            filter_strs: a list of strings against which the rows in the report should
+                be filtered. The queries are combined with a boolean "and".
         """
         self.headings = headings
         self.title = title
@@ -44,6 +48,7 @@ class Report:
         self.no_headings = no_headings
         self.no_borders = no_borders
         self.align = align
+        self.filter_strs = filter_strs
 
         # find the heading to sort on
         if sort_by is not None:
@@ -146,7 +151,9 @@ class Report:
         """Return a pretty-looking string from the data.
 
         Returns:
-            The contents formatted as a tabular-string.
+            a tuple containing the contents formatted as a
+            tabular-string, and any potential error messages which
+            occured while formatting or filtering.
         """
         pt = PrettyTable()
         pt.field_names = self.headings
@@ -173,8 +180,13 @@ class Report:
         for heading in self.headings:
             pt.align[heading] = self.align
 
-        for row in self.data:
-            pt.add_row(row.values())
+        try:
+            for row in filter_list(self.data, self.filter_strs):
+                pt.add_row(row.values())
+        except (KeyError, ParseError, TypeError) as err:
+            LOGGER.warning("An error occurred while filtering; returning no output."
+                           .format(err))
+            return ''
 
         return table_str + str(pt)
 

sat/showrev/main.py
@@ -74,7 +74,8 @@ def showrev(args):
         title = 'Installed Package Versions'
         headings = ['name', 'version']
         reports.append(Report(
-            headings, title, sort_by, reverse, no_headings, no_borders))
+            headings, title, sort_by, reverse, no_headings, no_borders,
+            filter_strs=args.filter_strs))
         reports[-1].add_rows(data)
 
     if args.format == 'yaml':

commit 309bb825288ad3a9ea65e4bfbf57fc9b655857f4

CRAYSAT-199: Fix issues with SAT_142.
Commit 9837bedede3 introduced some issues when using tabular output
when no filters were supplied, causing all commands to fail:

```
(venv) spottedcow-sms01-nmn:~/haasken/sat # sat status

WARNING: Session is not authenticated. Obtain a token with "auth" subcommand, or use --token-file on the command line.

WARNING: An error occurred while filtering; returning no output.
```

This change fixes that, and also fixes a missing-variable bug
introduced in that same commit. In addition, Docker and system version
tables did respect filter parameters which were added in SAT_142.
This change fixes that.

Test Description:
1. Run `sat hwinv --list-nodes --format yaml` and ensure it completes
   successfully.
2. Run `sat status` and ensure it runs successfully.
3. `sat showrev --system --filter 'component="System name"'`
4. `sat showrev --docker --filter 'name=sma*'`

sat/filtering.py
@@ -440,6 +440,9 @@ def filter_list(dicts, query_strings):
     if not dicts:
         return []
 
+    if not query_strings:
+        return dicts
+
     # Assume the first row's headings are the "right ones."
     first, rest = dicts[0], dicts[1:]
     fkeys = first.keys()

sat/hwinv/system.py
@@ -195,7 +195,7 @@ class System:
             else:
                 list_key = '{}_list'.format(object_type.arg_name)
                 all_lists[list_key] = filter_list(self.get_components_as_dicts(
-                    comp_dict.values(), fields), self.args.filter_str)
+                    comp_dict.values(), fields), self.args.filter_strs)
 
         return all_lists
 

sat/parsergroups.py
@@ -53,7 +53,8 @@ def create_filter_options():
         'filtering options', 'Options to filter output for applicable commands.')
 
     group.add_argument(
-        '--filter', metavar='QUERY', dest='filter_strs', action='append',
+        '--filter', metavar='QUERY', dest='filter_strs',
+        action='append', default=[],
         help='Filter the output against some constraint.')
 
     return parser

sat/report.py
@@ -48,7 +48,7 @@ class Report:
         self.no_headings = no_headings
         self.no_borders = no_borders
         self.align = align
-        self.filter_strs = filter_strs
+        self.filter_strs = filter_strs or []
 
         # find the heading to sort on
         if sort_by is not None:
@@ -151,9 +151,7 @@ class Report:
         """Return a pretty-looking string from the data.
 
         Returns:
-            a tuple containing the contents formatted as a
-            tabular-string, and any potential error messages which
-            occured while formatting or filtering.
+            The contents formatted as a tabular-string.
         """
         pt = PrettyTable()
         pt.field_names = self.headings
@@ -181,12 +179,16 @@ class Report:
             pt.align[heading] = self.align
 
         try:
-            for row in filter_list(self.data, self.filter_strs):
-                pt.add_row(row.values())
+            rows_to_print = filter_list(self.data, self.filter_strs)
+
         except (KeyError, ParseError, TypeError) as err:
-            LOGGER.warning("An error occurred while filtering; returning no output."
-                           .format(err))
+            LOGGER.warning("An error occurred while filtering; "
+                           "returning no output. (%s)", err)
             return ''
+
+        else:
+            for row in rows_to_print:
+                pt.add_row(row.values())
 
         return table_str + str(pt)
 
@@ -196,4 +198,13 @@ class Report:
         Returns:
             The data of the report formatted as a string in yaml format.
         """
-        return yaml_dump(self.data)
+        try:
+            rows_to_print = filter_list(self.data, self.filter_strs)
+
+        except (KeyError, ParseError, TypeError) as err:
+            LOGGER.warning("An error occurred while filtering; "
+                           "returning no output. (%s)", err)
+            return ''
+
+        else:
+            return yaml_dump(rows_to_print)

sat/showrev/main.py
@@ -49,7 +49,8 @@ def showrev(args):
         title = 'System Revision Information'
         headings = ['component', 'data']
         reports.append(Report(
-            headings, title, sort_by, reverse, no_headings, no_borders))
+            headings, title, sort_by, reverse, no_headings, no_borders,
+            filter_strs=args.filter_strs))
         reports[-1].add_rows(data)
 
     if args.docker:
@@ -62,7 +63,8 @@ def showrev(args):
         title = 'Installed Container Versions'
         headings = ['name', 'short-id', 'versions']
         reports.append(Report(
-            headings, title, sort_by, reverse, no_headings, no_borders))
+            headings, title, sort_by, reverse, no_headings, no_borders,
+            filter_strs=args.filter_strs))
         reports[-1].add_rows(data)
 
     if args.packages:

tests/test_filtering.py
@@ -176,6 +176,13 @@ class TestFilterList(unittest.TestCase):
         self.assertEqual(filtering.filter_list([], self.query_strings),
                          [])
 
+    def test_filtering_with_no_filters(self):
+        """Test filtering against an empty set of filters."""
+        items = [{'name': 'garfield'}, {'name': 'odie'},
+                 {'name': 'jon arbuckle'}]
+        self.assertEqual(filtering.filter_list(items, []),
+                         items)
+
     def test_invalid_input_list(self):
         """Test filtering a list with inconsistent headings (i.e. keys)."""
         with self.assertRaises(ValueError):