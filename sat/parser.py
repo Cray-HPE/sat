859aa12f512c3ab1bbf2f6e4aacc31d251c3d6e9
Jack Stanek <jstanek@cray.com>
2019-11-25 15:57:27 -0600

    CRAYSAT-218: Improve help messages for incorrect args in subcommands
    This change makes it so that when an unrecognized argument is passed
    to some subcommand on the command line, an appropriate help message is
    printed to stderr based on which subcommand is in use. Previously, a
    terse and relatively unhelpful error message was returned if an
    unrecognized argument was passed. Now, passing an unrecognized
    argument to some subcommand `sub` will have the same effect as running
    `sat sub --help`.
    
    Furthermore, this change improves error messages by describing how
    exactly the given arguments are invalid. Error messages tell users if
    they passed an invalid argument to the top level or to a subcommand,
    and will alert users if no subcommand was used.
    
    Test Description:
    
    Ran `sat` with numerous subcommands and invalid options. Tested `sat`
    with no arguments. Tested `sat` with no subcommand. Tested `sat` with
    no subcommand and invalid arguments.

--- sat/parser.py
+++ sat/parser.py
@@ -4,9 +4,95 @@ Functions to create the top-level ArgumentParser for the program.
 Copyright 2019 Cray Inc. All Rights Reserved.
 """
 
-from argparse import ArgumentParser
+from argparse import ArgumentError, ArgumentParser, _SubParsersAction
+import sys
+
+import inflect
 
 import sat.cli
+
+
+def _unrecognized_msg(unknown, subcommand=None):
+    """Generates an error message describing unrecognized option(s).
+
+    Args:
+        unknown ([str]): unrecognized arguments
+        subcommand (str, None): the subcommand in used.
+
+    Returns:
+        a string describing the unrecognized arguments.
+    """
+    inf = inflect.engine()
+    return ('unrecognized {}{}: {}'
+            .format(inf.plural("argument", len(unknown)),
+                    ' for subcommand {}'.format(subcommand) if subcommand else '',
+                    inf.join(unknown)))
+
+
+class SATArgParser(ArgumentParser):
+    """Small subclass of argparse.ArgumentParser.
+
+    This is used to automatically print usage information for subcommands. By
+    default, ArgumentParser will not print meaningful usage information for
+    subparsers when problematic arguments are given. The only difference this
+    subclass makes is that it *will* print a usage statement for subcommands
+    when they are given problematic arguments.
+    """
+    def parse_args(self, args=None, namespace=None):
+        """Parses command line arguments.
+
+        See superclass documentation.
+        """
+        parsed, unknown = self.parse_known_args(args, namespace)
+
+        if parsed.command is None:  # No subcommand given
+            self.print_help()
+            if unknown:
+                self.error(_unrecognized_msg(unknown))
+            else:
+                self.error('missing subcommand')
+
+        elif unknown:  # Some unknown arguments were given
+            self.error(_unrecognized_msg(unknown, subcommand=parsed.command))
+
+        else:
+            return parsed
+
+
+    def parse_known_args(self, args=None, namespace=None):
+        self.exit_on_error = False
+        return super().parse_known_args(args=args, namespace=namespace)
+
+
+    def error(self, message):
+        """Prints errors based on invalid arguments.
+
+        See superclass documentation.
+        """
+        if len(sys.argv) > 1:
+            subcommand = sys.argv[1]
+            subparser_actions = [action for action in self._actions
+                                 if isinstance(action, _SubParsersAction)]
+            if len(subparser_actions) < 1:
+                # If there aren't any subparsers, that indicates that
+                # this method is being called from the context of a
+                # subparser instance. Thus we can just print the usage
+                # for the "whole" parser.
+                self._print_message(self.format_usage(),
+                                    file=sys.stderr)
+            else:
+                # This method is being called from the top-level
+                # parser. There *should* only be one _SubparsersAction
+                # object in this instance, even though we don't
+                # enforce this. For simplicity, we can just take the
+                # first (only) one.
+                action = subparser_actions.pop()
+                if subcommand in action.choices:
+                    self._print_message(action.choices[subcommand].format_help(),
+                                        file=sys.stderr)
+
+        fargs = {'prog': self.prog, 'message': message}
+        self.exit(2, "{prog}: error: {message}\n".format(**fargs))
 
 
 def create_parent_parser():
@@ -17,7 +103,7 @@ def create_parent_parser():
         added to it.
     """
 
-    parser = ArgumentParser(description='SAT - The System Admin Toolkit')
+    parser = SATArgParser(description='SAT - The System Admin Toolkit')
 
     parser.add_argument(
         '-u', '--username',
