6c6517b7d0fc823c6debe89b182866421d4867a1
Ryan Haasken <haasken@cray.com>
2020-03-10 12:58:16 -0500

    CRAYSAT-289: Implement reading of release files in `sat showrev`
    The file `/opt/cray/etc/release` that used to contain version
    information about the products installed on a Shasta system was changed
    to a directory that contains files for each product specifying version
    information about that product. As a result `sat showrev --system` was
    failing with a traceback.
    
    Update the `sat showrev` command to parse the contents of any files it
    finds under the directory `/opt/cray/etc/release`. Since these files
    have multiple values in them, not just a single version number, and the
    output of `--system` only has two columns in the table, move this output
    to a new section of output controlled by a new `--products` option. Make
    `--system` and `--products` the default behavior of `sat showrev` if no
    options are specified.
    
    Implement the logic for parsing the contents of the release directory in
    a new `sat.cli.showrev.products` module, and write unit tests for this
    file to achieve 100% coverage. Remove old code for parsing singlular
    release file and unit tests for that old code.
    
    Make some minor man page and usage information cleanups, e.g. to
    consistently use the word 'display' rather than 'print'. Change exit
    calls to sys.exit and reference a JIRA to fix early exits.
    
    Test Description:
    All unit tests pass with 100% coverage of new `products.py` module. Ran
    `pycodestyle` and verified no style issues introduced. Copied to
    thanos-ncn-w001 and ran `sat showrev` with various combinations of
    options and verified the output matched what was in the file.

---
+++ tests/cli/showrev/test_products.py
@@ -0,0 +1,137 @@
+"""
+Tests for sat.cli.showrev.products module.
+
+Copyright 2020 Cray Inc. All Rights Reserved.
+"""
+import os
+import unittest
+from unittest.mock import patch
+
+from sat.cli.showrev.products import get_product_versions, RELEASE_FILE_COLUMN
+from sat.system.constants import MISSING_VALUE
+
+SAMPLES_DIR = os.path.join(os.path.dirname(__file__), 'samples')
+
+
+class TestProducts(unittest.TestCase):
+
+    def test_get_product_versions_good(self):
+        """Test get_product_versions with a good release directory."""
+        release_dir = os.path.join(SAMPLES_DIR, 'good_release')
+        headers, versions = get_product_versions(release_dir)
+
+        expected_headers = [
+            RELEASE_FILE_COLUMN,
+            'PRODUCT',
+            'OS',
+            'ARCH',
+            'VERSION',
+            'DATE',
+        ]
+        expected_versions = [
+            ['analytics',
+             "Cray's Analytics Programming Environment Packages",
+             'SLES15SP1',
+             'x86_64',
+             'base',
+             '20200309155039'],
+            ['cle',
+             "Cray's Linux Environment",
+             'SLES15SP1',
+             'x86_64',
+             '1.2.0',
+             '20200306143455'],
+            ['pe-base',
+             "Cray's Programming Environment BASE Packages",
+             'SLES15SP1',
+             'x86_64',
+             'master',
+             '20200309154143'],
+        ]
+
+        self.assertEqual(expected_headers, headers)
+        self.assertEqual(expected_versions, versions)
+
+    def test_get_product_versions_messy(self):
+        """Test get_product_versions with a messy release directory.
+
+        This release directory will contain an empty file, a normal product
+        version file, a product version file without all its typical keys and
+        a blank line, a product version file with an extra key, a file that
+        contains a bunch of garbage in it, and a nested directory that should
+        be ignored.
+
+        The get_product_versions function should handle all of this gracefully.
+        """
+        release_dir = os.path.join(SAMPLES_DIR, 'messy_release')
+        headers, versions = get_product_versions(release_dir)
+
+        expected_headers = [
+            RELEASE_FILE_COLUMN,
+            'PRODUCT',
+            'OS',
+            'ARCH',
+            'VERSION',
+            'DATE',
+            'EXTRA_STUFF'
+        ]
+        expected_versions = [
+            ['analytics',
+             "Cray's Analytics Programming Environment Packages",
+             'SLES15SP1',
+             MISSING_VALUE,
+             MISSING_VALUE,
+             MISSING_VALUE,
+             MISSING_VALUE],
+            ['cle',
+             "Cray's Linux Environment",
+             'SLES15SP1',
+             'x86_64',
+             '1.2.0',
+             '20200306143455',
+             'hello=goodbye'],
+            ['pe-base',
+             "Cray's Programming Environment BASE Packages",
+             'SLES15SP1',
+             'x86_64',
+             'master',
+             '20200309154143',
+             MISSING_VALUE],
+        ]
+
+        self.assertEqual(expected_headers, headers)
+        self.assertEqual(expected_versions, versions)
+
+    @patch('os.listdir', return_value=[])
+    def test_get_product_versions_empty_dir(self, _):
+        """Test get_product_versions with an empty release directory."""
+        headers, versions = get_product_versions('empty_dir')
+        self.assertEqual([], headers)
+        self.assertEqual([], versions)
+
+    @patch('os.listdir', side_effect=FileNotFoundError)
+    def test_get_product_versions_non_existent_dir(self, _):
+        """Test get_product_versions with a non-existent release dir"""
+        headers, versions = get_product_versions('does_not_exist')
+        self.assertEqual([], headers)
+        self.assertEqual([], versions)
+
+    @patch('os.listdir', side_effect=NotADirectoryError)
+    def test_get_product_versions_release_non_directory(self, _):
+        """Test get_product_versions with release dir not being a dir."""
+        headers, versions = get_product_versions('not_a_directory')
+        self.assertEqual([], headers)
+        self.assertEqual([], versions)
+
+    @patch('os.listdir', return_value=['bad_perms', 'other_unreadable'])
+    @patch('builtins.open', side_effect=[PermissionError, OSError])
+    @patch('os.path.isfile', return_value=True)
+    def test_get_product_versions_unreadable_files(self, *_):
+        """Test get_product_versions with a unreadable files."""
+        headers, versions = get_product_versions()
+        self.assertEqual([], headers)
+        self.assertEqual([], versions)
+
+
+if __name__ == '__main__':
+    unittest.main()
