fe47610046c18b2919327497873a774e7ada2319
Bo Cimino <bcimino@cray.com>
2020-05-08 14:18:52 -0500

    CRAYSAT-447: Count pods
    sat k8s now prints the ratio of co-located replicas to the number of
    running pods. Also contains a minor refactor to k8s with new unit
    tests.

--- tests/cli/k8s/test_k8s.py
+++ tests/cli/k8s/test_k8s.py
@@ -2,58 +2,34 @@
 Unit tests for sat.cli.k8s logic.
 
 (C) Copyright 2020 Hewlett Packard Enterprise Development LP.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import unittest
-from argparse import Namespace
 from unittest import mock
 
 from kubernetes.config.config_exception import ConfigException
 from kubernetes.client.rest import ApiException
 
 import sat.cli.k8s.main
-from sat.cli.k8s.main import ReplicaSet
-
-
-class FakeReplicaSet:
-    def __init__(self, name, namespace, hash_):
-        self.metadata = Namespace()
-        self.metadata.name = name
-        self.metadata.namespace = namespace
-        self.metadata.labels = {'pod-template-hash': hash_}
-
-
-class FakeReplicaSets:
-    """Mocks the return from list_replica_set_for_all_namespaces.
-    """
-    def __init__(self):
-        self.items = [
-            FakeReplicaSet('replica-set1', 'namespace', 'dupes-hash'),
-        ]
-
-
-class FakePod:
-    def __init__(self, name, node_name):
-        self.metadata = Namespace()
-        self.metadata.name = name
-
-        self.spec = Namespace()
-        self.spec.node_name = node_name
-
-        self.status = Namespace()
-        self.status.phase = 'Running'
-
-    def __str__(self):
-        print(self.metadata.name, self.spec.node_name)
-
-
-class FakePods:
-    def __init__(self):
-        self.items = [
-            FakePod('replica1-dupes-hash-1', 'w001'),
-            FakePod('replica1-dupes-hash-2', 'w001'),
-            FakePod('replica1-dupes-hash-3', 'w001'),
-        ]
+from sat.cli.k8s.replicaset import ReplicaSet
 
 
 class TestK8s(unittest.TestCase):
@@ -62,53 +38,21 @@ class TestK8s(unittest.TestCase):
     def tearDown(self):
         mock.patch.stopall()
 
-    def test_get_co_located_replicas(self):
-        """Positive test case for get_co_located_replicas.
-        """
-        mock.patch(
-            'sat.cli.k8s.main.kubernetes.config.load_kube_config',
-            return_value=None).start()
-
-        mock.patch(
-            'sat.cli.k8s.main.kubernetes.client.AppsV1Api.list_replica_set_for_all_namespaces',
-            return_value=FakeReplicaSets()).start()
-
-        mock.patch(
-            'sat.cli.k8s.main.kubernetes.client.CoreV1Api.list_namespaced_pod',
-            return_value=FakePods()).start()
-
-        replicas = sat.cli.k8s.main.get_co_located_replicas()
-
-        expected = ReplicaSet('namespace', 'replica-set1')
-        self.assertIn(expected, replicas)
-        self.assertEqual(3, len(replicas[expected]['w001']))
-
-        fake_pods = FakePods()
-        for e, a in zip(fake_pods.items, replicas[expected]['w001']):
-            self.assertEqual(e.metadata.name, a)
-
     def test_get_co_located_replicas_api_exception_config(self):
         """It should re-raise the same exception as load_kube_config.
         """
         mock.patch(
-            'sat.cli.k8s.main.kubernetes.config.load_kube_config',
+            'sat.cli.k8s.main.ReplicaSet.get_all_replica_sets',
             side_effect=ConfigException).start()
 
         with self.assertRaises(ConfigException):
             sat.cli.k8s.main.get_co_located_replicas()
 
     def test_get_co_located_replicas_api_exception_list_replicas(self):
-        """get_co_located_replicas should re-raise an ApiException.
-
-        ApiException can be raised by the call to
-        'list_replica_set_for_all_namespaces'.
+        """It should re-raise an ApiException.
         """
         mock.patch(
-            'sat.cli.k8s.main.kubernetes.config.load_kube_config',
-            return_value=None).start()
-
-        mock.patch(
-            'sat.cli.k8s.main.kubernetes.client.AppsV1Api.list_replica_set_for_all_namespaces',
+            'sat.cli.k8s.main.ReplicaSet.get_all_replica_sets',
             side_effect=ApiException).start()
 
         with self.assertRaises(ApiException):
