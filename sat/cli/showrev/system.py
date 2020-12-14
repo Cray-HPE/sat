c661fb3a69572d0f814c762751640fd2ae8ca9de
Eli Kamin <eli.kamin@hpe.com>
2020-12-14 16:30:56 -0600

    CRAYSAT-747: Fix error logging from S3 error
    Test Description:
    Intentionally mis-configured S3 endpoint and ran `sat showrev`,
    and checked that the error message was readable.

--- sat/cli/showrev/system.py
+++ sat/cli/showrev/system.py
@@ -71,7 +71,7 @@ def get_site_data(sitefile):
         LOGGER.debug('Downloading %s from S3 (bucket: %s)', sitefile, s3_bucket)
         s3.Object(s3_bucket, sitefile).download_file(sitefile)
     except (BotoCoreError, ClientError, Boto3Error) as err:
-        LOGGER.error('Unable to download site info file %s from S3. Attempting to read from cached copy.'
+        LOGGER.error('Unable to download site info file %s from S3. Attempting to read from cached copy. '
                      'Error: %s', sitefile, err)
 
     try:
