8fd583cc9c96fc72e0bd56265500ec20c6620782
Eli Kamin <eli.kamin@hpe.com>
2021-03-22 18:54:35 -0500

    CRAYSAT-853: Isolate dependencies for docker builds
    * Add build scripts runBuildPrepVenv.sh and runUnitTestVenv.sh
    * Move all build prep scripts to a sub-directory called build_scripts/
    * Update Jenkinsfile.docker to use the new "Venv" scripts in the new
      location
    * Update Jenkinsfile.rpm to use the same scripts, but in the new
      location
    
    Test Description:
    After pushing this branch, ran the build manually several times.

---
+++ build_scripts/runBuildPrepVenv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# (C) Copyright 2021 Hewlett Packard Enterprise Development LP.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+base_dir=$(dirname "$0")
+rm -rf sat-venv && virtualenv -p $(which python3) ./sat-venv
+source sat-venv/bin/activate
+$base_dir/runBuildPrep.sh
