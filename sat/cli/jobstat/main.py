50231e41f0613d0144e3f710432634696c6ebb3f
elmir-dzaka <elmir.dzaka@hpe.com>
2022-08-08 17:10:00 +0000

    PE-42104: Add jobstat to SAT CLI
    Commit Changes:
    - This commit implements `sat jobstat`. This command gets a system-wide view of
      applications and displays it in a pretty table format. This commit also adds
      a change log entry and man page for the command.
    
    Review feedback:
    - Refactored code according to comments by using APIGatewayClient
      subclass instead of using requests directly.
    - Change URL from localhost to API gateway.
    - Rename client class to JobstatClient.
    - Use single quotes instead of double quotes.
    - Checking for error code and jobstat key in the server response.
    - Formatting fixed on the man page.
    - Word wrap to 80 characters.
    - Indented example in the man page.
    
    Test Description:
    - Tested code by running state checker server in the demo and non-demo modes and
      running `sat jobstat` on local machine pointed to Slice.

---
+++ sat/cli/jobstat/main.py
@@ -0,0 +1,62 @@
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+"""
+The main entry point for the jobstat subcommand.
+"""
+import logging
+from sat.report import Report
+from sat.config import get_config_value
+from sat.apiclient import APIError
+from sat.apiclient.jobstat import JobstatClient
+from sat.session import SATSession
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def do_jobstat(args):
+    """Run the jobstat command with the given arguments.
+    Args:
+        args: The argparse.Namespace object containing the parsed arguments
+            passed to this subcommand.
+    """
+    state_checker_client = JobstatClient(SATSession())
+    try:
+        application_data = state_checker_client.get_all()
+        if not application_data:
+            return
+        report = Report(
+            tuple(application_data[0].keys()),
+            None,
+            args.sort_by,
+            args.reverse,
+            filter_strs=args.filter_strs,
+            display_headings=args.fields,
+            print_format=args.format,
+        )
+        for row in application_data:
+            report.add_row(row)
+        print(report)
+    except APIError as err:
+        LOGGER.error(err)
+        raise SystemExit(1)
