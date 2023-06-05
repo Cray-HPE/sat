7371206b37404617a22ad72c6e7abc004a0dae88
Ryan Haasken <ryan.haasken@hpe.com>
2023-06-05 16:39:21 -0500

    CRAYSAT-1718: Allow bootprep to specify arch for BOS session templates
    Add the ability to specify the architecture for a BOS session template,
    both at the top level and at the individual boot set level in the `sat
    bootprep` input file. Pass these fields along verbatim to BOS when
    creating session templates.
    
    Add unit tests for the `get_create_item_data` method of the
    `InputSessionTemplate` class to validate correctness.
    
    Test Description:
    New unit tests are passing. This has not yet been validated on a real
    system.

---
+++ tests/cli/bootprep/input/test_session_template.py
@@ -0,0 +1,189 @@
+#
+# MIT License
+#
+# (C) Copyright 2023 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+"""
+Tests for functionality in the InputSessionTemplate class and subclasses
+"""
+from jinja2.sandbox import SandboxedEnvironment
+import unittest
+from unittest.mock import Mock
+
+from sat.cli.bootprep.input.session_template import InputSessionTemplateV2
+from sat.cli.bootprep.errors import InputItemValidateError
+
+
+class TestInputSessionTemplateV2(unittest.TestCase):
+
+    def setUp(self):
+        self.input_instance = Mock()
+        self.index = 0
+        self.jinja_env = SandboxedEnvironment()
+        self.bos_client = Mock()
+        self.mock_base_boot_set_data = {
+            'rootfs_provider': 'cpss3'
+        }
+        self.bos_client.get_base_boot_set_data.return_value = self.mock_base_boot_set_data
+        self.cfs_client = Mock()
+        self.ims_client = Mock()
+
+        self.mock_image_id = '17b649e6-94b3-4ede-a179-d911a1c26468'
+        self.mock_etag = '20dd8b6d42071198195134ef8e1619d8'
+        self.mock_path = f's3://boot-images/{self.mock_image_id}/manifest.json'
+        self.mock_image_type = 's3'
+        self.mock_image_link = {
+            'etag': self.mock_etag,
+            'path': self.mock_path,
+            'type': self.mock_image_type
+        }
+        test_self = self
+
+        class SimplifiedInputSessionTemplateV2(InputSessionTemplateV2):
+            """A simplified version of MockInputSessionTemplateV2.
+
+            Useful for testing methods that use these methods without testing these methods.
+            """
+
+            def validate_image_exists(self, **_):
+                """Simplified validation that only support IMS images by name"""
+                if self.ims_image_name:
+                    return {'name': self.ims_image_name}
+                else:
+                    raise InputItemValidateError(f'{self.__class__} only supports '
+                                                 f'IMS images specified by name.')
+
+            @property
+            def image_record(self):
+                """Simplified version that does not actually query IMS."""
+                image_record = self.validate_image_exists()
+                image_record['id'] = test_self.mock_image_id
+                image_record['link'] = test_self.mock_image_link
+                return image_record
+
+        self.simplified_session_template_v2 = SimplifiedInputSessionTemplateV2
+
+    def get_input_and_expected_bos_data(self, name='my-session-template',
+                                        image_name='my-image', configuration='my-config',
+                                        arch=None, arch_by_bootset=None):
+        """Get input data and the expected BOS request payload with given parameters"""
+        if not arch_by_bootset:
+            arch_by_bootset = {'compute': None}
+
+        input_bootsets = {}
+        bos_payload_bootsets = {}
+        for boot_set_name, boot_set_arch in arch_by_bootset.items():
+            input_bootsets[boot_set_name] = {
+                'node_roles_groups': ['Compute']
+            }
+            bos_payload_bootsets[boot_set_name] = {
+                'node_roles_groups': ['Compute'],
+                'path': self.mock_path,
+                'etag': self.mock_etag,
+                'type': self.mock_image_type
+            }
+            bos_payload_bootsets[boot_set_name].update(self.mock_base_boot_set_data)
+            if boot_set_arch:
+                input_bootsets[boot_set_name]['arch'] = boot_set_arch
+                bos_payload_bootsets[boot_set_name]['arch'] = boot_set_arch
+
+        input_data = {
+            'name': name,
+            'image': {'ims': {'name': image_name}},
+            'configuration': configuration,
+            'bos_parameters': {
+                'boot_sets': input_bootsets
+            }
+        }
+        bos_payload = {
+            'name': name,
+            'cfs': {'configuration': configuration},
+            'enable_cfs': True,
+            'boot_sets': bos_payload_bootsets
+        }
+
+        if arch:
+            input_data['arch'] = arch
+            bos_payload['arch'] = arch
+
+        return input_data, bos_payload
+
+    def test_get_create_item_data_no_arch(self):
+        """Test get_create_item_data method with no architecture specified"""
+        input_data, expected_bos_data = self.get_input_and_expected_bos_data()
+
+        input_session_template = self.simplified_session_template_v2(
+            input_data, self.input_instance, 0, self.jinja_env,
+            self.bos_client, self.cfs_client, self.ims_client
+        )
+
+        self.assertEqual(expected_bos_data,
+                         input_session_template.get_create_item_data())
+
+    def test_get_create_item_data_top_level_arch(self):
+        """Test get_create_item_data method with architecture specified at top level"""
+        input_data, expected_bos_data = self.get_input_and_expected_bos_data(arch='aarch64')
+
+        input_session_template = self.simplified_session_template_v2(
+            input_data, self.input_instance, 0, self.jinja_env,
+            self.bos_client, self.cfs_client, self.ims_client
+        )
+
+        self.assertEqual(expected_bos_data,
+                         input_session_template.get_create_item_data())
+
+    def test_get_create_item_data_arch_per_bootset(self):
+        """Test get_create_item_data with a different arch per boot set"""
+        input_data, expected_bos_data = self.get_input_and_expected_bos_data(
+            arch_by_bootset={'compute_x86_64': 'x86_64',
+                             'compute_aarch64': 'aarch64'}
+        )
+
+        input_session_template = self.simplified_session_template_v2(
+            input_data, self.input_instance, 0, self.jinja_env,
+            self.bos_client, self.cfs_client, self.ims_client
+        )
+
+        self.assertEqual(expected_bos_data,
+                         input_session_template.get_create_item_data())
+
+    def test_get_create_item_data_arch_with_bootset_override(self):
+        """Test get_create_item_data with a top-level arch overridden in one boot set."""
+        input_data, expected_bos_data = self.get_input_and_expected_bos_data(
+            arch='x86_64',
+            arch_by_bootset={
+                # Results in it not being specified in the input data for this boot set
+                'compute_x86_64': None,
+                # Results in it being specified in the input data for this boot set
+                'compute_aarch64': 'aarch64'}
+        )
+
+        input_session_template = self.simplified_session_template_v2(
+            input_data, self.input_instance, 0, self.jinja_env,
+            self.bos_client, self.cfs_client, self.ims_client
+        )
+
+        self.assertEqual(expected_bos_data,
+                         input_session_template.get_create_item_data())
+
+
+if __name__ == '__main__':
+    unittest.main()
