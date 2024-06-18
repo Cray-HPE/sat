fea1a4103be91b3cb86e5d4c44b1d6e2cd86b519
Ryan Haasken <ryan.haasken@hpe.com>
2024-06-18 17:10:12 -0500

    CRAYSAT-1861: Add functions to run lsof and find ceph/s3fs mounts
    Add functions that check whether mounts are ready to be unmounted via
    `lsof` and find the ceph and s3fs mounts.
    
    This is an untested work in progress still.

--- sat/cli/bootsys/filesystems.py
+++ sat/cli/bootsys/filesystems.py
@@ -27,12 +27,12 @@ Manage the mounting and unmounting of filesystems during shutdown and boot.
 from collections import namedtuple
 import json
 import logging
-import shlex
 
 from paramiko.ssh_exception import BadHostKeyException, AuthenticationException, SSHException
 
 from sat.cli.bootsys.hostkeys import FilteredHostKeys
 from sat.cli.bootsys.util import get_and_verify_ncn_groups, get_ssh_client, FatalBootsysError
+from sat.util import prompt_continue
 
 LOGGER = logging.getLogger(__name__)
 
@@ -62,7 +62,7 @@ def find_rbd_device_mounts(ssh_client, hostname):
     ssh_client.connect(hostname)
 
     rbd_list_cmd = 'rbd device list --format json'
-    stdin, stdout, stderr = ssh_client.exec_command(shlex.split(rbd_list_cmd))
+    stdin, stdout, stderr = ssh_client.exec_command(rbd_list_cmd)
     exit_code = stdout.channel.recv_exit_status()
     if exit_code:
         raise FilesystemError(
@@ -82,7 +82,7 @@ def find_rbd_device_mounts(ssh_client, hostname):
         LOGGER.info(f'Checking for mounts of RBD device {device_path} on {hostname}')
 
         findmnt_cmd = f'findmnt --source {device_path} --output TARGET -n'
-        stdin, stdout, stderr = ssh_client.exec_command(shlex.split(findmnt_cmd))
+        stdin, stdout, stderr = ssh_client.exec_command(findmnt_cmd)
         exit_code = stdout.channel.recv_exit_status()
 
         rbd_mount_point = stdout.read()
@@ -96,3 +96,84 @@ def find_rbd_device_mounts(ssh_client, hostname):
     return mounted_rbd_devices
 
 
+def find_ceph_and_s3fs_mounts(ssh_client, hostname):
+    """Find Ceph and s3fs mounts.
+
+    Args:
+        ssh_client (paramiko.SSHClient): the SSH client to use to connect to the host
+        hostname (str): the hostname to connect to
+
+    Raises:
+        FilesystemError: if there is a problem finding ceph or s3fs mount points
+
+    Returns:
+        list of str: the list of mount points of ceph and s3fs filesystems
+    """
+    ssh_client.connect(hostname)
+
+    # Finds filesystems of type ceph or fuse.s3fs and only outputs the TARGET with no headers
+    findmnt_cmd = 'findmnt --types ceph,fuse.s3fs --output TARGET -n'
+    stdin, stdout, stderr = ssh_client.exec_command(findmnt_cmd)
+    exit_code = stdout.channel.recv_exit_status()
+    if exit_code:
+        raise FilesystemError(
+            f'Command "{findmnt_cmd}" failed to list mounted ceph or s3fs '
+            f'filesystems on {hostname}: {stderr.read()}'
+        )
+
+    return stdout.read().decode().splitlines()
+
+
+def prepare_umount(ssh_client, hostname, mount_points):
+    """Check whether mounts are active or not.
+
+    For each given mount point on the given hostname, check whether the mount
+    point is currently in use, and if so, give the user an opportunity to stop
+    any processes using the mount point.
+
+        ssh_client (paramiko.SSHClient): the SSH client to use to connect to the host
+        hostname (str): the hostname to connect to
+        mount_points (list of str): the list of mount points to check for activity
+
+    Raises:
+        FilesystemError: if there is a problem listing RBD devices or mount points
+
+    Returns:
+        list of RBDDeviceMount: the list of mounted RBD devices with their path
+            and mount point
+    """
+    ssh_client.connect(hostname)
+
+    while True:
+        mount_points_in_use = False
+        for mount_point in mount_points:
+
+            lsof_cmd = f'lsof {mount_point}'
+
+            LOGGER.info(f'Checking whether mount point {mount_point} is in use.')
+
+            stdin, stdout, stderr = ssh_client.exec_command(lsof_cmd)
+            exit_code = stdout.channel.recv_exit_status()
+
+            # lsof exits with non-zero exit status when mount point is not in use
+            if exit_code:
+                LOGGER.info(f'Mount point {mount_point} is not in use.')
+                continue
+
+            # lsof exits with zero exit status when mount point is in use
+            mount_points_in_use = True
+            LOGGER.info(f'Mount point {mount_point} is in use by the following processes:')
+            LOGGER.info(f'{stdout.read()}')
+
+        if mount_points_in_use:
+            prompt_continue(
+                'umount of filesystems',
+                'Some filesystems to be unmounted remain in use. '
+                'Please address this before continuing.'
+            )
+        elif mount_points:
+            LOGGER.info('All mount points are ready to be unmounted.')
+            break
+        else:
+            LOGGER.debug('No mount points to unmount.')
+            break
