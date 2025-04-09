ae42dc657958067fc5b2bcd3bb0e13377313c958
ethanholen-hpe <ethan.holen@hpe.com>
2025-04-09 13:46:34 -0600

    CRAYSAT-1690: Always print report even when some items fail

--- tests/cli/bootprep/test_main.py
+++ tests/cli/bootprep/test_main.py
@@ -1,7 +1,7 @@
 #
 # MIT License
 #
-# (C) Copyright 2021-2024 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2021-2025 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -45,6 +45,8 @@ from sat.cli.bootprep.main import (
     do_bootprep_schema
 )
 from sat.cli.bootprep.validate import SCHEMA_FILE_RELATIVE_PATH
+
+from csm_api_client.k8s import LOGGER
 
 
 class TestDoBootprepDocs(unittest.TestCase):
@@ -153,6 +155,9 @@ class TestDoBootprepRun(unittest.TestCase):
 
     def test_do_bootprep_run_success(self):
         """Test do_bootprep_run in the successful case"""
+        created_images = [MagicMock()]
+        failed_images = []
+        self.mock_create_images.return_value = created_images, failed_images
         with self.assertLogs(level=logging.INFO) as cm:
             do_bootprep_run(self.mock_validator_cls, self.args)
 
@@ -245,15 +250,15 @@ class TestDoBootprepRun(unittest.TestCase):
 
     def test_do_bootprep_run_image_create_error(self):
         """Test do_bootprep_run when an error occurs creating an image"""
-        create_err_msg = 'Failed to create an image'
-        self.mock_create_images.side_effect = ImageCreateError(create_err_msg)
+        created_images = []
+        failed_images = [MagicMock()]
+        self.mock_create_images.return_value = created_images, failed_images
+
         with self.assertRaises(SystemExit) as raises_cm:
             with self.assertLogs(level=logging.ERROR) as logs_cm:
                 do_bootprep_run(self.mock_validator_cls, self.args)
 
         self.assertEqual(1, raises_cm.exception.code)
-        self.assertEqual(1, len(logs_cm.records))
-        self.assertEqual(create_err_msg, logs_cm.records[0].msg)
         self.mock_load_and_validate_instance.assert_called_once_with(
             self.input_file, self.mock_validator_cls)
         self.mock_input_instance_cls.assert_called_once_with(
@@ -268,11 +273,6 @@ class TestDoBootprepRun(unittest.TestCase):
         self.mock_configurations.create_items.assert_called_once_with()
         self.mock_validate_images.assert_called_once_with(self.mock_input_instance, self.args, self.mock_cfs_client)
         self.mock_create_images.assert_called_once_with(self.mock_input_instance, self.args, self.mock_ims_client)
-        self.mock_session_templates.handle_existing_items.assert_not_called()
-        self.mock_session_templates.validate.assert_not_called()
-        self.mock_session_templates.create_items.assert_not_called()
-
-        self.mock_multireport_cls.assert_not_called()
 
 
 class TestDoBootprep(unittest.TestCase):
