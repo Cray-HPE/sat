eb2cfadeb277f6c9accee18550c2579f10311844
annapoorna-s <annapoorna.s@hpe.com>
2025-04-11 10:49:57 +0530

    CRAYSAT-1988: Unit tests for duplicate image prompt
    (cherry picked from commit 4fdb962289476022310f4347a7c41a907edd0e03)

--- tests/cli/bootprep/input/test_base.py
+++ tests/cli/bootprep/input/test_base.py
@@ -1,7 +1,7 @@
 #
 # MIT License
 #
-# (C) Copyright 2022-2024 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2022-2025 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -79,6 +79,22 @@ class TestBaseInputItem(unittest.TestCase):
                 'site-domain': 'example.com'
             }
         }
+        self.mock_data = {
+            "name": "test-item",
+            "type": "test-type",
+            "if_exists": None,  # Default value for if_exists
+        }
+
+        # Create a concrete subclass of BaseInputItem for testing
+        class ConcreteBaseInputItem(BaseInputItem):
+            """Dummy implementation of BaseInputItem for testing."""
+            def get_create_item_data(self):
+                return {}
+
+            def create(self, payload):
+                pass
+
+        self.ConcreteBaseInputItem = ConcreteBaseInputItem
 
     def test_report_rows_have_correct_attrs(self):
         """Test that report rows are generated for input items"""
@@ -186,3 +202,35 @@ class TestBaseInputItem(unittest.TestCase):
                      r"for value complex_data: 'colors' is undefined")
         with self.assertRaisesRegex(item_cls.template_render_err, err_regex):
             item.complex_data
+
+    def test_if_exists_skip(self):
+        """Test BaseInputItem behavior when if_exists is set to 'skip'."""
+        self.mock_data["if_exists"] = "skip"
+        item = self.ConcreteBaseInputItem(self.mock_data, self.mock_instance, 0, self.jinja_env)
+
+        # Ensure the if_exists attribute is set correctly
+        self.assertEqual(item.if_exists, "skip")
+
+    def test_if_exists_overwrite(self):
+        """Test BaseInputItem behavior when if_exists is set to 'overwrite'."""
+        self.mock_data["if_exists"] = "overwrite"
+        item = self.ConcreteBaseInputItem(self.mock_data, self.mock_instance, 0, self.jinja_env)
+
+        # Ensure the if_exists attribute is set correctly
+        self.assertEqual(item.if_exists, "overwrite")
+
+    def test_if_exists_abort(self):
+        """Test BaseInputItem behavior when if_exists is set to 'abort'."""
+        self.mock_data["if_exists"] = "abort"
+        item = self.ConcreteBaseInputItem(self.mock_data, self.mock_instance, 0, self.jinja_env)
+
+        # Ensure the if_exists attribute is set correctly
+        self.assertEqual(item.if_exists, "abort")
+
+    def test_if_exists_default(self):
+        """Test BaseInputItem behavior when if_exists is not set."""
+        del self.mock_data["if_exists"]  # Remove the if_exists key
+        item = self.ConcreteBaseInputItem(self.mock_data, self.mock_instance, 0, self.jinja_env)
+
+        # Ensure the if_exists attribute defaults to None
+        self.assertIsNone(item.if_exists)
